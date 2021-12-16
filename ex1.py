cbf3b943601563c3879358c82a3490a22d26ab90
Kevin Johnson <kevijohn@gmail.com>
2021-12-16 11:32:38 -0500

    Initial commit of first 3 exercise files

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# Author: Kevin Johnson
+# Created: DEC 15 2021
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drills
+print("Print some text on\na new line.")
