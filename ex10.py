bf264c67f6e532cb7d50abe95a6a8917770d4810
Kevin Johnson <kevijohn@gmail.com>
2021-12-17 18:30:56 -0500

    Today's exercises

---
+++ ex10.py
@@ -0,0 +1,18 @@
+# Author: Kevin Johnson
+# Created: DEC 17 2021
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
