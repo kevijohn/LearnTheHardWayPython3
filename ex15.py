7dda4f36c89e01a31ffca0ea494a83535e92b9c0
Kevin Johnson <kevijohn@gmail.com>
2021-12-18 17:25:13 -0500

    Today's exercises

---
+++ ex15.py
@@ -0,0 +1,18 @@
+# Author: Kevin Johnson
+# Created: DEC 18 2021
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+text_again = open(file_again)
+
+print(text_again.read())
