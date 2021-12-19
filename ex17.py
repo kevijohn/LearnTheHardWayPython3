973aba0d9a244fcc94bb0062b1620133421af665
Kevin Johnson <kevijohn@gmail.com>
2021-12-19 18:22:35 -0500

    Today's exercises

--- ex17.py
+++ ex17.py
@@ -9,8 +9,9 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # We could do these two on one line, how?
-in_file = open(from_file)
-inData = in_file.read()
+#in_file = open(from_file)
+#inData = in_file.read()
+inData = open(from_file).read()
 
 print(f"The input file is {len(inData)} bytes long")
 
@@ -24,4 +25,5 @@ out_file.write(inData)
 print("Alright, all done.")
 
 out_file.close()
-in_file.close()
+#in_file.close()
+
