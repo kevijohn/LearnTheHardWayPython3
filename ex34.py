02cbc1b4479898cf216126d384df52c9b0e24e43
Kevin Johnson <kevijohn@gmail.com>
2021-12-24 18:44:04 -0500

    Today's exercises

---
+++ ex34.py
@@ -0,0 +1,13 @@
+# Author: Kevin Johnson
+# Created: DEC 24 2021
+
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("animal at 1 is:\t", animals[1])   # python3.6
+print("3rd animal is:\t", animals[2])    # peacock
+print("1st animal is:\t", animals[0])    # bear
+print("animal at 3 is: ", animals[3])   # kangaroo
+print("5th animal is: ", animals[4])    # whale
+print("animal at 2 is: ", animals[2])   # peacock
+print("6th animal is: ", animals[5])    # platypus
+print("animal at 4 is: ", animals[4])   # whale
---
+++ ex36.py
@@ -0,0 +1,13 @@
+# Author: Kevin Johnson
+# Created: DEC 24 2021
+
+# I don't have time to make a game right now
+
+# ok, how about one where I choose to study or take care of my kid?
+
+# choose study and > 5 hours, die, too long, kid starved
+# study < 5 hours, ok, better to take care of kid, branch to kid
+# choose kid and is hungry, choose feed, play, or study
+# feed kid, is happy
+# play, kid is happy but starved
+# study, branch to study
