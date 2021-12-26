b61219a3ef49237916b09e3f686082e8877dc8f5
Kevin Johnson <kevijohn@gmail.com>
2021-12-26 16:13:48 -0500

    Today's exercises

---
+++ ex42.py
@@ -0,0 +1,5 @@
+# Author: Kevin Johnson
+# Created: DEC 26 2021
+
+# skipping this one for now.  There's nothing executable
+# from my end, it's just entering comments.
---
+++ ex43.py
@@ -0,0 +1,4 @@
+# Author: Kevin Johnson
+# Created: DEC 26 2021
+
+# This is going to take a while
